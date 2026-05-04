685667e720e75530684847d8ddc7823e1b3362c2
jessicaandradeortega <doraortega737@gmail.com>
2026-05-04 11:16:19 -0500

    Create excepciones.py
    Se agregan excepciones personalizadas del sistema

---
+++ core/utils/excepciones.py
@@ -0,0 +1,19 @@
+class ClienteError(Exception):
+    """
+    Excepción personalizada para errores relacionados con clientes.
+    """
+    pass
+
+
+class ServicioError(Exception):
+    """
+    Excepción personalizada para errores relacionados con servicios.
+    """
+    pass
+
+
+class ReservaError(Exception):
+    """
+    Excepción personalizada para errores relacionados con reservas.
+    """
+    pass
